594734b4ed22bcd166a6b39e966c03a79ef90499
sinhvien <sinhvien>
2020-06-06 10:28:34 +0700

    asdds

---
+++ Tuan3/src/Tuan3/Enc_RSA_2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tuan3;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Enc_RSA_2 {
+    public static void main (String args[]) throws Exception{
+        String s = "Hello World!";
+        FileInputStream f = new FileInputStream("D:\\Skey_RSA_pub.dat");
+        ObjectInputStream b = new ObjectInputStream(f);
+        RSAPublicKey pbk = (RSAPublicKey)b.readObject();
+        BigInteger e = pbk.getPublicExponent();
+        BigInteger n = pbk.getModulus();
+        System.out.println("e="+e);
+        System.out.println("n"+n);
+        byte ptext[] = s.getBytes("UTF8");
+        BigInteger m = new BigInteger(ptext);
+        BigInteger c = m.modPow(e, n);
+        System.out.println("c="+c);
+        String cs = c.toString();
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\Enc_RSA.dat")));
+        out.write(cs,0,cs.length());
+        out.close(); 
+    }
+}
